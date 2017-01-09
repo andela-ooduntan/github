e38ca445d560757e9854061f83bceff1168b53fb
Oduntan Oluwatobiloba Stephen <stephen.oduntan@andela.com>
2017-01-09 23:18:38 +0100

    Fix bud some bugs

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@ var express = require('express');
 var request = require('request');
 var router = express.Router();
 
-// var createHandler = require('github-webhook-handler');
-// var handler = createHandler({ path: 'https://api.github.com/repos/andela-ooduntan/Riten/hooks', secret: 'oluwatobiloba' })
-var access_token = 'a1775fe6a0224ace9152be6df100c912d89eea98';
+var access_token = 'f7a9d79ba65b11248c0604ba1260b406fafcf4a6';
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var url = "https://api.github.com/repos/andela-ooduntan/Riten/issues?per_page=100&page=1&sort=created&state=open&access_token=" + access_token;
@@ -15,37 +13,20 @@ router.get('/', function(req, res, next) {
         'User-Agent': 'request'
       }
     }, function (error, response, body) {
-      console.log( JSON.parse(body)[0])
+      console.log( JSON.parse(body))
       res.render('index', { issues: JSON.parse(body) });
 
 
     })
 
-  // handler(req, res, function (err) {
-  //   console.log(err, 'this is error')
-  //   // res.statusCode = 404
-  //   // res.end('no such location')
-  // })
 
 });
 
 router.post('/', function(req, res) {
   res.io.emit("new-issue", res.req.body);
   res.send('respond with a resource.');
-  // res.render('index', { issues: reactiveIssue });
 })
 
-// handler.on('issues', function (event) {
-//   console.log('Received an issue event for %s action=%s: #%d %s',
-//     event.payload.repository.name,
-//     event.payload.action,
-//     event.payload.issue.number,
-//     event.payload.issue.title)
-// })
-
-// handler.on('error', function (err) {
-//   console.error('Error:', err.message)
-// })
 
 
 module.exports = router;
